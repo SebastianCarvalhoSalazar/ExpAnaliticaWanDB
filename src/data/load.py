34948e23a9a7810c0331e27c68f6903215d6947b
SebastianCarvalhoSalazar <sebastian.carvalho2596@gmail.com>
2024-02-10 16:49:38 -0500

    add: features

--- src/data/load.py
+++ src/data/load.py
@@ -16,7 +16,7 @@ args = parser.parse_args()
 if args.IdExecution:
     print(f"IdExecution: {args.IdExecution}")
 
-def load(train_size=.8):
+def load(train_size=.8, test_size=.1):
     """
     Load the Wisconsin Breast Cancer dataset.
     """
@@ -24,33 +24,47 @@ def load(train_size=.8):
     wbcd = wisconsin_breast_cancer_data = datasets.load_breast_cancer()
     feature_names = wbcd.feature_names
     labels = wbcd.target_names
-    X_train, X_test, y_train, y_test = train_test_split(wbcd.data, wbcd.target, test_size=0.2)
-    return [X_train, X_test, y_train, y_test, feature_names, labels]
+    # Split data into train and test sets
+    X_train, X_test, y_train, y_test = train_test_split(wbcd.data, wbcd.target, test_size=(1-train_size))
+    # Further split test set into test and validation sets
+    X_test, X_validation, y_test, y_validation = train_test_split(X_test, y_test, test_size=test_size)
+
+    return {
+        'Data': {
+            'Train': {'X': X_train, 'y': y_train},
+            'Validation': {'X': X_validation, 'y': y_validation},
+            'Test': {'X': X_test, 'y': y_test}
+        },
+        'Metadata': {'Feature_names': feature_names, 'Labels': labels}
+    }
 
 def load_and_log():
     # 🚀 start a run, with a type to label it and a project it can call home
     with wandb.init(
         project="expanaliticawandb",
         name=f"Load Raw Data ExecId-{args.IdExecution}", job_type="load-data") as run:
-        
         datasets = load()  # separate code for loading the datasets
-        print(datasets[0])
 
-        # 🏺 create our Artifact
+        # Create a W&B Artifact
         raw_data = wandb.Artifact(
-            "mnist-raw", type="dataset",
-            description="raw wisconsin_breast_cancer_dataset, split into train/val/test",
-            metadata={"source": "sklearn.datasets.load_breast_cancer",
-                      "sizes": [len(dataset) for dataset in datasets]})
+            "wisconsin_breast_cancer_dataset-raw", type="dataset",
+            description="raw Wisconsin Breast Cancer dataset, split into train/val/test",
+            metadata={
+                "source": "sklearn.datasets.load_breast_cancer",
+                "sizes": {key: (len(value['X']),len(value['y'])) for key, value in datasets['Data'].items()},
+                "features": len(datasets['Metadata']['Feature_names']),
+                "labels": len(datasets['Metadata']['Labels'])
+            })
+        
+        # Save each dataset split to the artifact
+        for name, data in datasets['Data'].items():
+            with raw_data.new_file(name + "_X.npy", mode="wb") as file:
+                np.save(file, data['X'])
+            with raw_data.new_file(name + "_y.npy", mode="wb") as file:
+                np.save(file, data['y'])        
 
-        # for name, data in zip(names, datasets):
-        #     # 🐣 Store a new file in the artifact, and write something into its contents.
-        #     with raw_data.new_file(name + ".pt", mode="wb") as file:
-        #         x, y = data.tensors
-        #         torch.save((x, y), file)
-
-        # # ✍️ Save the artifact to W&B.
-        # run.log_artifact(raw_data)
+        # Save the artifact to W&B
+        run.log_artifact(raw_data)                
 
 # testing
 load_and_log()
